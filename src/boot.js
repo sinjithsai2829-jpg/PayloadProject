d636304ec5e3d1ef336336776ec846e25e36bc0c
sinjithsai2829-jpg <sinjithsai2829@gmail.com>
2026-09-11 22:21:07 -0400

    Load wrapped indentation guide guard

--- src/boot.js
+++ src/boot.js
@@ -5,6 +5,7 @@ import './payload-auto-detect.js';
 import './paste-buttons.js';
 import './enhancements.js';
 import './word-wrap.js';
+import './wrap-indent-guide-guard.js';
 import './editable-default.js';
 import './sync-scroll.js';
 import './diff-nav-ui.js';
