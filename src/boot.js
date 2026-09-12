8bd9ecce00b213471a3ac636ec879e0a4b05b2eb
sinjithsai2829-jpg <sinjithsai2829@gmail.com>
2026-09-12 00:21:56 -0400

    Boot unified editor scroll geometry

--- src/boot.js
+++ src/boot.js
@@ -35,6 +35,7 @@ import './persistence.js';
 import './diff-visibility.js';
 import './editable-code-surface.js';
 import './scrollbar-visibility.js';
+import './editor-scroll-geometry.js';
 import './saved-comparisons.js';
 import './editor-syntax-markers.js';
 import './theme-diagnostics-contrast.js';
